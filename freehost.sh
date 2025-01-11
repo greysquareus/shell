109e10bc11bfb1edc9e5c258f62cb4b36b9c9b60
greysquareus <greysquareus@gmail.com>
2025-01-11 11:08:06 +0200

    Update freehost.sh

--- freehost.sh
+++ freehost.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-$PASSWORD = q1w2e3r4t5y6
-$MYSQL_PASSWORD = root
-$IP = icanhazip=`wget -q -4 -O- http://icanhazip.com`
+PASSWORD="q1w2e3r4t5y6"
+MYSQL_PASSWORD="root"
+IP=$(wget -q -4 -O- http://icanhazip.com)
 
 sudo apt update -y
 sleep 3
+
 sudo apt install lamp-server^ -y
 sleep 3
-sudo mysql -u root -p 
-$MYSQL_PASSWORD 
-sleep 3
-create database wp_db default character set utf 8 ;
-sleep 3
-create user 'wp_user'@'localhost' identified by '$PASSWORD'
-sleep 3
-grant all on wp_db.* to 'wp_user'@'localhost';
-sleep 3
-flush privileges;
-exit
+
+sudo mysql -u root -p"$MYSQL_PASSWORD" -e "CREATE DATABASE wp_db DEFAULT CHARACTER SET utf8;"
+sudo mysql -u root -p"$MYSQL_PASSWORD" -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '$PASSWORD';"
+sudo mysql -u root -p"$MYSQL_PASSWORD" -e "GRANT ALL PRIVILEGES ON wp_db.* TO 'wp_user'@'localhost';"
+sudo mysql -u root -p"$MYSQL_PASSWORD" -e "FLUSH PRIVILEGES;"
+
 sleep 5
+
 sudo apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-zip php-xmlrpc php-mysql php-cli -y 
 sleep 3
+
 sudo wget -c http://wordpress.org/latest.tar.gz
 sleep 3
 sudo tar -xzvf latest.tar.gz 
 sleep 3
-sudo rsinc -av wordpress/* var/www/html
-sudo chown -R www-data:www:data /var/www/html
+sudo rsync -av wordpress/* /var/www/html
+sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 755 /var/www/html
 sudo rm /var/www/html/index.html
+
+sudo systemctl restart apache2
